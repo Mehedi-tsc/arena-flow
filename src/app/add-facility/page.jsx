30d39287d0f8c85fcab12a9f12ca856297d250a9
Mehedi-tsc <mehedimmc1@gmail.com>
2026-05-18 20:52:28 +0600

    Add facility form added

--- src/app/add-facility/page.jsx
+++ src/app/add-facility/page.jsx
@@ -1,11 +1,119 @@
-import React from 'react';
+"use client";
+import { FloppyDisk } from "@gravity-ui/icons";
+import {
+  Button,
+  Description,
+  FieldError,
+  Fieldset,
+  Form,
+  Input,
+  Label,
+  Surface,
+  TextArea,
+  TextField,
+} from "@heroui/react";
 
-const AddFacility = () => {
-    return (
-        <div className='max-w-7xl mx-auto'>
-           Add Facility 
-        </div>
-    );
+const AddFacilitiesPage = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+  };
+  return (
+    <div className=" rounded-2xl bg-surface p-6 my-6 max-w-5xl mx-auto shadow-sm">
+      <Surface className="w-full min-w-95">
+        <Form onSubmit={onSubmit}>
+          <Fieldset className="w-full">
+            <Fieldset.Legend className="text-3xl">Add Facility</Fieldset.Legend>
+            <Description>Publish your facility and get booked by sports enthusiasts.</Description>
+            <Fieldset.Group>
+              <div className="flex flex-col lg:flex-row lg:gap-10">
+                <div className="space-y-2">
+                  <TextField
+                    isRequired
+                    name="facilityName"
+
+                  >
+                    <Label>Facility Name</Label>
+                    <Input placeholder="Enter Facility Name" variant="secondary" />
+                    <FieldError />
+                  </TextField>
+                  <TextField
+                    isRequired
+                    name="image"
+
+                  >
+                    <Label>Image URL</Label>
+                    <Input placeholder="Enter Image URL" variant="secondary" />
+                    <FieldError />
+                  </TextField>
+                  <TextField
+                    isRequired
+                    name="price"
+
+                  >
+                    <Label>Price Per Hour ($)</Label>
+                    <Input placeholder="Enter Your Price in Doller" variant="secondary" />
+                    <FieldError />
+                  </TextField>
+                </div>
+                <div className="space-y-3">
+                  <TextField
+                    isRequired
+                    name="facilityType"
+
+                  >
+                    <Label>Facility Type</Label>
+                    <Input placeholder="Enter Facility Type" variant="secondary" />
+                    <FieldError />
+                  </TextField>
+                  <TextField
+                    isRequired
+                    name="location"
+
+                  >
+                    <Label>Location</Label>
+                    <Input placeholder="Enter location" variant="secondary" />
+                    <FieldError />
+                  </TextField>
+                  <TextField
+                    isRequired
+                    name="capacity"
+
+                  >
+                    <Label>Capacity</Label>
+                    <Input placeholder="Enter Your Capacity" variant="secondary" />
+                    <FieldError />
+                  </TextField>
+                </div>
+              </div>
+              <TextField isRequired name="timeSlot" type="text">
+                <Label>Available Time Slots</Label>
+                <Input placeholder="Enter The Available Time Slots" variant="secondary" />
+                <FieldError />
+              </TextField>
+              <TextField
+                isRequired
+                name="description"
+                
+              >
+                <Label>Description</Label>
+                <TextArea placeholder="Tell us about your facility..." variant="secondary" />
+                
+                <FieldError />
+              </TextField>
+            </Fieldset.Group>
+            <Fieldset.Actions>
+              <Button type="submit">
+                <FloppyDisk />
+                Add Facility
+              </Button>
+              
+            </Fieldset.Actions>
+          </Fieldset>
+        </Form>
+      </Surface>
+    </div>
+  );
 };
 
-export default AddFacility;
+export default AddFacilitiesPage;
