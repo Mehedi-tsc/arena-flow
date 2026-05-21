a4869509129bdd290533d4ef315c6789dcf19265
Mehedi-tsc <mehedimmc1@gmail.com>
2026-05-21 09:08:38 +0600

    Search functionality added

--- src/app/all-facilities/page.jsx
+++ src/app/all-facilities/page.jsx
@@ -1,14 +1,46 @@
+import Search from "@/components/Search";
 import FacilityCard from "@/lib/FacilityCard";
 
 
-const AllFacilitiesPage = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URI}/facilities`)
-    const facilities =await res.json()
+const AllFacilitiesPage = async ({searchParams}) => {
+    const resolvedParams = await searchParams;
+    const search = resolvedParams?.search || "";
+    
+    
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URI}/facilities?search=${search}`, 
+        {cache: "no-store"}
+    )
+    const facilities = await res.json()
     return (
         <div className='max-w-7xl mx-auto my-5'>
-            <h2 className="font-bold text-3xl">All Facilities</h2>
+            <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4 mb-8">
+
+                <h1 className="text-3xl font-bold">
+                    All Facilities
+                </h1>
+
+                <div className="flex flex-col sm:flex-row gap-3">
+                    {/* search */}
+                    <Search/>
+
+                    {/* Filter */}
+                    <select
+                        // value={sportType}
+                        // onChange={(e) => setSportType(e.target.value)}
+                        className="border border-gray-300 rounded-xl px-4 py-2 outline-none focus:ring-2 focus:ring-blue-500"
+                    >
+                        <option value="">All Sports</option>
+                        <option value="Football">Football</option>
+                        <option value="Cricket">Cricket</option>
+                        <option value="Swimming">Swimming</option>
+                        <option value="Badminton">Badminton</option>
+                        <option value="Rowing">Rowing</option>
+                    </select>
+
+                </div>
+            </div>
             <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-                {facilities.map(facility=><FacilityCard key={facility._id} facility={facility}></FacilityCard>)}
+                {facilities.map(facility => <FacilityCard key={facility._id} facility={facility}></FacilityCard>)}
             </div>
 
         </div>
---
+++ src/components/Search.jsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+
+const Search = () => {
+    const [search, setSearch]= useState("")
+    const router = useRouter()
+    const searchParams = useSearchParams()
+    const handleSearch =()=>{
+        const params = new URLSearchParams(searchParams.toString());
+        if(search){
+            params.set("search", search)
+        }else{
+            params.delete("search")
+        }
+        router.push(`/all-facilities?${params.toString()}`)
+    }
+    
+    return (
+        <div className="flex items-center gap-2">
+
+                        <input
+                            type="text"
+                            placeholder="Search facility..."
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                            className="border border-gray-300 rounded-xl px-4 py-2 outline-none focus:ring-2 focus:ring-blue-500"
+                        />
+
+                        <button
+                            onClick={handleSearch}
+                            className="bg-blue-600 hover:bg-blue-700 text-white px-5 py-2 rounded-xl font-medium transition"
+                        >
+                            Search
+                        </button>
+
+                    </div>
+    );
+};
+
+export default Search;
